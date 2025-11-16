15f1f0b44212b1079182680946aa8c0578fdb078
Srafi3107 <srafi3107@gmail.com>
2025-11-16 11:05:39 +0600

    Updated Playwright test and project files

--- login_add_cart_logout.spec.js
+++ login_add_cart_logout.spec.js
@@ -2,21 +2,17 @@ const { test, expect, chromium } = require('@playwright/test');
 
 test('Login, add product to cart, verify, and logout', async () => {
 
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
 
-
   await page.goto('https://www.saucedemo.com/');
-
 
   await page.fill('#user-name', 'standard_user');
   await page.fill('#password', 'secret_sauce');
   await page.click('#login-button');
 
-
   await page.click('button[data-test="add-to-cart-sauce-labs-onesie"]');
-
 
   await page.click('.shopping_cart_link');
   const productName = await page.textContent('.inventory_item_name');
